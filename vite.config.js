3e3d01b7a436b44ce8132ccb1f33741a498008d0
Isaac Ferreira <zaclummys@gmail.com>
2025-06-24 22:42:37 -0300

    Add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: 'https://zaclummys.github.io/guess-piano-key-game/',
   assetsInclude: [
     'src/mp3s/*.mp3',
   ],
