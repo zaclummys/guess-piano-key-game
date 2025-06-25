552f4a0ec3746830335416a5e1b063c04adfde11
Isaac Ferreira <zaclummys@gmail.com>
2025-06-24 22:39:12 -0300

    Fix app import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './styles.css';
 
-import App from './App.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
